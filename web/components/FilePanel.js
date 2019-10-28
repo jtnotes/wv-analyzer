3daa06869a36ee597a11771459b6d0fcedf88b4a
jtnotes <dev@jtnotes.com>
2019-10-28 00:08:45 -0400

    add file selection button;

--- web/components/FilePanel.js
+++ web/components/FilePanel.js
@@ -23,7 +23,22 @@ class FilePanel extends React.Component {
 
                 <div id="navbarBasicExample" class="navbar-menu">
                     <div class="navbar-start">
-                        <a class="navbar-item">Home</a>
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <div class="file has-name">
+                                    <label class="file-label">
+                                        <input class="file-input" type="file" name="resume" />
+                                        <span class="file-cta">
+                                            <span class="file-icon">
+                                                <i class="fas fa-upload"></i>
+                                            </span>
+                                            <span class="file-label">                                                        Choose a file…      </span>
+                                        </span>
+                                        <span class="file-name">                                                    Screen Shot 2017-07-29 at 15.54.25.png    </span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
 
                         <a class="navbar-item">Documentation</a>
 
